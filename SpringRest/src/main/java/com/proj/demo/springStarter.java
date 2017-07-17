7b406bf81f22072e791fc7d2cd64390bcb235044
prateekt91 <prat91@gmail.com>
2017-07-18 01:15:37 +0530

    Add files via upload

---
+++ SpringRest/src/main/java/com/proj/demo/springStarter.java
@@ -0,0 +1,15 @@
+package com.proj.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class springStarter {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(springStarter.class, args);
+
+	}
+
+}
